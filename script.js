64bd3f12a1f874b27a6f0bd60d1d2d43b75ef0e7
IsaacSeb <sebbieisaac720@gmail.com>
2025-08-11 12:33:04 +0300

    Call playRound with 2 arguments hS + cS * hS = humanSelection variable which is a function Call * cS = computerSelection variable which is also a function Call

--- script.js
+++ script.js
@@ -59,3 +59,5 @@ function playRound(humanChoice, computerChoice){
 
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
